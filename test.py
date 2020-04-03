f446805dd3e234e6117cfc030bac3161f5836178
vkorneychuk <blaker09@mail.ru>
2020-04-03 16:55:53 +0300

    prapare help command

---
+++ test.py
@@ -0,0 +1,6 @@
+import io
+
+path_to_help = './files/help.txt'
+
+help_file = open(path_to_help,'r')
+help_string = help_file.read()
--- vkorneychuk_ekivoki_bot.py
+++ vkorneychuk_ekivoki_bot.py
@@ -3,37 +3,29 @@ from classes.BotHandler import BotHandler
 
 token = "1173528624:AAE_VmquAJvssAkON6TVtAW_dQS0kTjMWZs"
 
-greet_bot = BotHandler(token)  
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-now = datetime.datetime.now()
+ekivoki_bot = BotHandler(token)  
+commands = ('/help', '/start', '/new_game', '/invite_friends', '/new_team') 
+path_to_help_file = './files/help.txt'
 
 
 def main():  
     new_offset = None
-    today = now.day
-    hour = now.hour
 
     while True:
-        greet_bot.get_updates(new_offset)
+        ekivoki_bot.get_updates(new_offset)
 
-        last_update = greet_bot.get_last_update()
+        last_update = ekivoki_bot.get_last_update()
 
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
+        if last_chat_text.lower() == '/help':
+            help_file = open(path_to_help,'r')
+            help_string = help_file.read()
+            ekivoki_bot.send_message(last_chat_id, help_string)
 
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
 
         new_offset = last_update_id + 1
 
